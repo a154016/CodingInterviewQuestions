987bde421d7341c920651aaef8591f7a3a39481b
Ashish <Ashish@192.168.1.41>
2021-12-07 22:01:27 +0530

    Adding more answers

--- CodingInterviewQuestions/src/com/ashish/interview/LargestSubArray.java
+++ CodingInterviewQuestions/src/com/ashish/interview/LargestSubArray.java
@@ -7,7 +7,7 @@ public class LargestSubArray {
 		int[] inputArray=new int[] {0,-1,1,4,5,-2};
 		for(int i=0;i<inputArray.length-1;i++)
 		{
-			for(int j=0;j<inputarray.length-1;j++)
+			for(int j=0;j<inputArray.length-1;j++)
 			{
 				
 			}
